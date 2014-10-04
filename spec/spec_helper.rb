8fd6ce6259d5adeb9c1bf74be53ec5f0b590b063
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-10-03 20:33:12 -0500

    make poltergeist timeout more liberal

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,13 @@ require "rspec/rails"
 require "capybara/poltergeist"
 
 Capybara.javascript_driver = :poltergeist
+options = {
+  js_errors: false,
+  timeout: 180,
+  phantomjs_options: ['--ignore-ssl-errors=yes']
+}
 Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, js_errors: false)
+  Capybara::Poltergeist::Driver.new(app, options)
 end
 
 require "database_cleaner"
