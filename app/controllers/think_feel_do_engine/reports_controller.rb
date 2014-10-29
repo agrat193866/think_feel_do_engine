05e4299a765ea5511d768263923e5a7af46fe35d
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-10-29 14:38:45 -0500

    added report generation to the admin interface

---
+++ app/controllers/think_feel_do_engine/reports_controller.rb
@@ -0,0 +1,30 @@
+module ThinkFeelDoEngine
+  # Provide access to data exports.
+  class ReportsController < ApplicationController
+    before_action :authenticate_user!
+
+    layout "manage"
+
+    def index
+      @reports = [
+        { title: "Lesson Slide View", id: :lesson_slide_view },
+        { title: "Lesson Viewing", id: :lesson_viewing },
+        { title: "Module Page View", id: :module_page_view },
+        { title: "Module Session", id: :module_session },
+        { title: "Site Session", id: :site_session }
+      ]
+    end
+
+    def show
+      report = {
+        lesson_slide_view: Reports::LessonSlideView,
+        lesson_viewing: Reports::LessonViewing,
+        module_page_view: Reports::ModulePageView,
+        module_session: Reports::ModuleSession,
+        site_session: Reports::SiteSession
+      }[params[:id].to_sym]
+
+      send_data report.to_csv, type: "text/csv"
+    end
+  end
+end
