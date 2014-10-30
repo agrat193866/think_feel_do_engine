23e25de52114cd11f1d267f28a01f605eb7531de
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-10-30 09:44:27 -0500

    add video sessions to reports [Finishes #81395412]

--- app/controllers/think_feel_do_engine/reports_controller.rb
+++ app/controllers/think_feel_do_engine/reports_controller.rb
@@ -11,7 +11,8 @@ module ThinkFeelDoEngine
         { title: "Lesson Viewing", id: :lesson_viewing },
         { title: "Module Page View", id: :module_page_view },
         { title: "Module Session", id: :module_session },
-        { title: "Site Session", id: :site_session }
+        { title: "Site Session", id: :site_session },
+        { title: "Video Session", id: :video_session }
       ]
     end
 
@@ -21,7 +22,8 @@ module ThinkFeelDoEngine
         lesson_viewing: Reports::LessonViewing,
         module_page_view: Reports::ModulePageView,
         module_session: Reports::ModuleSession,
-        site_session: Reports::SiteSession
+        site_session: Reports::SiteSession,
+        video_session: Reports::VideoSession
       }[params[:id].to_sym]
 
       send_data report.to_csv, type: "text/csv"
