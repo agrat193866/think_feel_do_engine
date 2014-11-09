6f7d64d642ee38e6b73fc35e9caadc8a274cc954
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-11-09 14:10:47 -0600

    remove requirement to view intro slideshow on first login [Finishes #79183850]

--- app/controllers/think_feel_do_engine/participant_authentication_policy.rb
+++ app/controllers/think_feel_do_engine/participant_authentication_policy.rb
@@ -7,16 +7,7 @@ module ThinkFeelDoEngine
     end
 
     def post_sign_in_path
-      if @participant.sign_in_count == 1 &&
-         (slideshow = SlideshowAnchor.fetch(:home_intro))
-        ThinkFeelDoEngine::Engine.routes.url_helpers
-          .participants_slideshow_slide_path(
-            slideshow_id: slideshow.id,
-            id: slideshow.slides.first.id
-          )
-      else
-        "/"
-      end
+      "/"
     end
   end
 end
--- spec/features/participant/first_login_slideshow_spec.rb
+++ spec/features/participant/first_login_slideshow_spec.rb
@@ -8,17 +8,6 @@ feature "first login slideshow" do
       sign_in_participant participants(:participant1)
     end
 
-    it "should display the first slide of the 'Home Intro' slideshow" do
-      expect(page).to have_text "Welcome to ThiFeDo"
-    end
-  end
-
-  context "on second login" do
-    before do
-      sign_in_participant participants(:participant1)
-      sign_in_participant participants(:participant1)
-    end
-
     it "should display the home page" do
       expect(page).not_to have_text "Welcome to ThiFeDo"
       expect(page).to have_link "Replay Intro"
