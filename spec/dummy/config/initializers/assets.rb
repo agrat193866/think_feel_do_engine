f240e0a2ed5e553087005cf45eb7e3e8d395133b
michaelwehrley <michaelwehrley@gmail.com>
2014-11-26 12:05:49 -0600

    added tests for hiding provider linkes and tools based.

--- spec/dummy/config/initializers/assets.rb
+++ spec/dummy/config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( think_feel_do_engine.js think_feel_do_engine.css jquery.js think_feel_do_engine/devise/passwords/edit.js think_feel_do_engine/users.js engine.js engine.css )
+Rails.application.config.assets.precompile += %w( think_feel_do_engine.js think_feel_do_engine.css jquery.js think_feel_do_engine/devise/passwords/edit.js think_feel_do_engine/users.js engine.js engine.css)
--- spec/features/content_author/bit_maker/content_provider_spec.rb
+++ spec/features/content_author/bit_maker/content_provider_spec.rb
@@ -1,1 +1,22 @@
-# content_provider_spec.rb
+require "spec_helper"
+
+feature "Content Provider", type: :feature do
+  fixtures(
+    :arms, :participants, :users, :user_roles, :"bit_core/slideshows", :"bit_core/slides",
+    :"bit_core/tools", :"bit_core/content_modules",
+    :"bit_core/content_providers", :groups, :memberships,
+    :tasks, :task_status
+  )
+
+  before do
+    sign_in_user users :admin1
+    visit "/arms/#{arms(:arm1).id}/bit_maker/content_providers/#{bit_core_content_providers(:awake_period_form_provider).id}"
+  end
+
+  it "shoud have display a link to modules - not providers" do
+    expect(page).to have_text "Content Provider"
+    expect(page).to have_text "#1 Awareness"
+    expect(page).to_not have_link "Providers"
+    expect(page).to have_link "Module", href: "/arms/#{arms(:arm1).id}/bit_maker/content_modules/#{bit_core_content_modules(:do_awareness).id}"
+  end
+end
