e2754af40806fdb8b19ad2dc3b74a128ce58b31b
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-12-16 08:14:44 -0600

    update tfd engine; cop out on User password change redirect [#84243402] [#83687588]

--- app/controllers/think_feel_do_engine/users/registrations_controller.rb
+++ app/controllers/think_feel_do_engine/users/registrations_controller.rb
@@ -1,8 +1,8 @@
+# Customize User registrations.
 class RegistrationsController < Devise::RegistrationsController
-
   protected
 
-  def after_update_path_for(resource)
-    arms_path
+  def after_update_path_for(_resource)
+    "/arms"
   end
 end
