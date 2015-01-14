9a0c62f88dd5a850b72746a83dd95581109dd819
Eric Schlange <eric.schlange@gmail.com>
2015-01-14 11:04:04 -0600

    [Finished #85444664] Modified site-wide date formats to include full year in all cases where year is visible.

--- config/initializers/datetime_formats.rb
+++ config/initializers/datetime_formats.rb
@@ -1,14 +1,14 @@
 Time::DATE_FORMATS.merge!(
   date_time_with_meridian: ->(t) { 
     if t.year != Date.today.year
-      t.strftime("%b. %e '%y - %l:%M %P")
+      t.strftime("%b. %e %Y - %l:%M %P")
     else
       t.strftime("%b. %e - %l:%M %P")
     end
   },
   brief_date: ->(t) { 
     if t.year != Date.today.year
-      t.strftime("%b %e '%y")
+      t.strftime("%b %e %Y")
     else
       t.strftime("%b %e")
     end
@@ -26,7 +26,7 @@ Time::DATE_FORMATS.merge!(
 Date::DATE_FORMATS.merge!(
   brief_date: ->(d) {
     if d.year != Date.today.year
-      d.strftime("%b %e '%y")
+      d.strftime("%b %e %Y")
     else
       d.strftime("%b %e")
     end
