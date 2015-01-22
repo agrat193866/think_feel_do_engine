a1e17058627bbec27f3d21dff3f977b37d842c6c
michaelwehrley <michaelwehrley@gmail.com>
2015-01-22 16:01:27 -0600

    updated authorization for researchers and removed functionality from the coach [Finishes: #86146446]

---
+++ spec/dummy/app/models/think_feel_do_dashboard/reports.rb
@@ -0,0 +1,29 @@
+module ThinkFeelDoDashboard
+  module Reports
+    # Models necessary for ability file to compile in specs.
+    report_classes = %w(
+      Comment
+      Goal
+      LessonModule
+      LessonSlideView
+      LessonViewing
+      Like
+      Login
+      ModulePageView
+      ModuleSession
+      Nudge
+      OffTopicPost
+      SiteSession
+      TaskCompletion
+      ToolAccess
+      ToolModule
+      ToolShare
+      UserAgent
+      VideoSession
+    )
+
+    report_classes.each do |klass|
+      const_set(klass, nil)
+    end
+  end
+end
--- spec/features/researcher/managing_tasks_spec.rb
+++ spec/features/researcher/managing_tasks_spec.rb
@@ -11,7 +11,7 @@ feature "managing tasks", type: :feature do
   )
 
   context "Logged in as a content author" do
-    let(:content_author1) { users(:content_author1) }
+    let(:researcher1) { users(:researcher1) }
     let(:participant3) { participants(:participant3) }
     let(:group1) { groups(:group1) }
     let(:group2) { groups(:group2) }
@@ -27,7 +27,7 @@ feature "managing tasks", type: :feature do
     let(:feel) { bit_core_content_modules(:feeling_tracker_module3) }
 
     before do
-      sign_in_user content_author1
+      sign_in_user researcher1
     end
 
     context "On new" do
@@ -114,7 +114,7 @@ feature "managing tasks", type: :feature do
 
         expect(page).not_to have_content("Tracking Your Mood")
 
-        sign_in_user content_author1
+        sign_in_user researcher1
         visit urls.arm_manage_tasks_group_path(group3.arm, group3)
 
         expect(page).to have_the_table(
