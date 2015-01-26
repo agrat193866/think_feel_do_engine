4019da427f3dccb5b12d7a90d2f369384ad348ee
Eric Carty-Fickes <ericcf@northwestern.edu>
2015-01-26 13:39:54 -0600

    move third party assets to vendor folder [#86799400]

--- app/assets/javascripts/think_feel_do_engine.js
+++ app/assets/javascripts/think_feel_do_engine.js
@@ -16,12 +16,12 @@
 //= require bootstrap/collapse
 //= require dataTables/jquery.dataTables
 //= require dataTables/jquery.dataTables.bootstrap3
-//= require think_feel_do_engine/snap
+//= require snap
 //= require moment.min
 //= require d3.min
 //
 //= require_tree ./think_feel_do_engine/shared
-//= require think_feel_do_engine/bootstrap-timepicker
+//= require bootstrap-timepicker
 //= require think_feel_do_engine/messages
 //= require think_feel_do_engine/scrollindicator
 //= require think_feel_do_engine/validate_social_sharing
