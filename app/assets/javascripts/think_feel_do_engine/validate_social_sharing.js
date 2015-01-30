65e58425fc174e3b9eb5ce8b2afba67704f5a50f
Tag0Mag0 <jem.hilton@gmail.com>
2015-01-30 15:58:53 -0600

    added back social validation checks to multiform js function
    [#87134200]

--- app/assets/javascripts/think_feel_do_engine/validate_social_sharing.js
+++ app/assets/javascripts/think_feel_do_engine/validate_social_sharing.js
@@ -1,6 +1,8 @@
 /**
  * Created by eric schlange on 12/16/14.
  */
+var validatedSocialSubmit = false;
+
 var validatePublic = function(event, shared_item_name) {
   // check if social sharing is included
   if ($('.'+shared_item_name).length > 0 && $('.'+shared_item_name+':checked').length > 0) {
@@ -19,20 +21,25 @@ var validatePublic = function(event, shared_item_name) {
 };
 
 var validateMutliFormPage = function(event, shared_item_name, activity_id) {
-  var shared_item_buttons = $("#activity-"+activity_id+" ."+shared_item_name);
-  if (shared_item_buttons != null && 0 < shared_item_buttons.length) {
-    for (var current_button_index = 0; current_button_index < shared_item_buttons.length && !validatedSocialSubmit; current_button_index++) {
-      if (shared_item_buttons[current_button_index].checked) {
-        if (confirm('Are you sure that you would like to make this activity public?')) {
-          return true;
-        } else {
-          event.preventDefault();
-          return false;
+  validatedSocialSubmit = false;
+  if(!validatedSocialSubmit) {
+    var shared_item_buttons = $("#activity-"+activity_id+" ."+shared_item_name);
+    if (shared_item_buttons != null && 0 < shared_item_buttons.length) {
+      for (var current_button_index = 0; current_button_index < shared_item_buttons.length && !validatedSocialSubmit; current_button_index++) {
+        if (shared_item_buttons[current_button_index].checked) {
+          if (confirm('Are you sure that you would like to make this activity public?')) {
+            validatedSocialSubmit = true;
+            return true;
+          } else {
+            event.preventDefault();
+            return false;
+          }
         }
       }
+    } else {
+      validatedSocialSubmit = true;
+      return true;
     }
-  } else {
-    return true;
   }
 }
 
