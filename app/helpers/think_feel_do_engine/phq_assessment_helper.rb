0acab34299ca67358aa1896ebb127089123fd445
michaelwehrley <michaelwehrley@gmail.com>
2015-01-30 09:54:34 -0600

    helper and partial displays phq9 notices [#87254192]

--- app/helpers/think_feel_do_engine/phq_assessment_helper.rb
+++ app/helpers/think_feel_do_engine/phq_assessment_helper.rb
@@ -10,5 +10,51 @@ module ThinkFeelDoEngine
         score
       end
     end
+
+    def phq_notice_copied(tested_weeks, notices)
+      if tested_weeks.map(&:missing_but_copied).include?(true)
+        notices << "#{fa_icon("copy")} \
+        PHQ9 assessment missing this week \
+        - values copied from previous assessment."
+      end
+    end
+
+    def phq_notice_lost(tested_weeks, notices)
+      if tested_weeks.map(&:missing_with_no_fallback).include?(true)
+        notices << "#{fa_icon("ban")} \
+        PHQ9 assessment missing this week \
+        - no previous assessment data to copy from."
+      end
+    end
+
+    def phq_notice_missing_some_answers(tested_weeks, notices)
+      question_counts = tested_weeks.map(&:missing_answers_count)
+      if question_counts.include?(1) ||
+        question_counts.include?(2) ||
+        question_counts.include?(3)
+        notices << "#{fa_icon("question")} \
+        PHQ9 assessment missing answers for up to 3 questions \
+        - using 1.5 to fill them in."
+      end
+    end
+
+    def phq_notice_unreliable(tested_weeks, notices)
+      question_counts = tested_weeks.map(&:missing_answers_count)
+      disqualify_array = Array(4..9)
+      if !(question_counts & disqualify_array).empty?
+        notices << "#{fa_icon("question-circle")} \
+        PHQ9 assessment missing answers for \
+        more than 3 questions - data unreliable."
+      end
+    end
+
+    def phq_notices(assessments)
+      notices = []
+      phq_notice_copied(assessments, notices)
+      phq_notice_lost(assessments, notices)
+      phq_notice_missing_some_answers(assessments, notices)
+      phq_notice_unreliable(assessments, notices)
+      notices.uniq
+    end
   end
 end
