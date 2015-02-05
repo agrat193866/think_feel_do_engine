082a64dbd57eb663d27e1b0540a4dcb6feb8c1c8
Tag0Mag0 <jem.hilton@gmail.com>
2015-02-05 11:54:01 -0600

    refactored markdown js code for word cleaner
    [#87640044]

--- app/assets/javascripts/think_feel_do_engine/markdown_editor.js
+++ app/assets/javascripts/think_feel_do_engine/markdown_editor.js
@@ -1,13 +1,13 @@
-var swapCodes   = new Array(8211, 8212, 8216, 8217, 8220, 8221, 8226, 8230); // dec codes from char at
-var swapStrings = new Array("--", "--", "'",  "'",  "\"",  "\"",  "*",  "...");  
+window.cleanWordClipboard = function(input){
+  var swapCodes   = new Array(8211, 8212, 8216, 8217, 8220, 8221, 8226, 8230); // dec codes from char at
+  var swapStrings = new Array("--", "--", "'",  "'",  "\"",  "\"",  "*",  "...");  
 
-function cleanWordClipboard(input) {
-    // debug for new codes
-    // for (i = 0; i < input.length; i++)  alert("'" + input.charAt(i) + "': " + input.charCodeAt(i));    
-    var output = input;
-    for (i = 0; i < swapCodes.length; i++) {
-        var swapper = new RegExp("\\u" + swapCodes[i].toString(16), "g"); // hex codes
-        output = output.replace(swapper, swapStrings[i]);
-    }
-    return output;
+  // debug for new codes
+  // for (i = 0; i < input.length; i++)  alert("'" + input.charAt(i) + "': " + input.charCodeAt(i));    
+  var output = input;
+  for (i = 0; i < swapCodes.length; i++) {
+      var swapper = new RegExp("\\u" + swapCodes[i].toString(16), "g"); // hex codes
+      output = output.replace(swapper, swapStrings[i]);
+  }
+  return output;
 }
