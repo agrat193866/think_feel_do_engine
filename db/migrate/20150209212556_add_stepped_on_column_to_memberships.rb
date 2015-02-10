18bf2a0a4d5b3c112ffc3ba809f9cbe6ede035d7
gwensmu <gwen.smuda@gmail.com>
2015-02-10 10:48:21 -0600

    [#87752774] fix mistake in seed during migration, correctly reference memberships instead of participants

--- db/migrate/20150209212556_add_stepped_on_column_to_memberships.rb
+++ db/migrate/20150209212556_add_stepped_on_column_to_memberships.rb
@@ -2,7 +2,7 @@ class AddSteppedOnColumnToMemberships < ActiveRecord::Migration
   def change
     add_column :memberships, :stepped_on, :date
 
-    Membership.where("memberships.is_stepped = ?", true) do |membership|
+    Membership.where("is_stepped = ?", true) do |membership|
       membership.stepped_on = Date.today
       membership.save
     end
