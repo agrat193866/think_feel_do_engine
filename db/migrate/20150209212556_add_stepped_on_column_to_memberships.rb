a92418419325f4b8f983969a55abd8d5701a5686
gwensmu <gwen.smuda@gmail.com>
2015-02-10 10:12:35 -0600

    [#87752774] revise migration seeding to be more succint for stepped_on schema change

--- db/migrate/20150209212556_add_stepped_on_column_to_memberships.rb
+++ db/migrate/20150209212556_add_stepped_on_column_to_memberships.rb
@@ -2,12 +2,9 @@ class AddSteppedOnColumnToMemberships < ActiveRecord::Migration
   def change
     add_column :memberships, :stepped_on, :date
 
-    Participant.each do |participant|
-      if participant.is_stepped
-        participant.stepped_on = Date.today
-        participant.save
-      end
+    Participant.where("memberships.is_stepped = ?", true) do |participant|
+      participant.stepped_on = Date.today
+      participant.save
     end
-
   end
 end
