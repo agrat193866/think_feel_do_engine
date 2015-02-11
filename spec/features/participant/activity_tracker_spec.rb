3a0f01f5780ea9ce8df8a4ae1ee0dfd419603b9b
Eric Schlange <eric.schlange@gmail.com>
2015-02-10 19:40:17 -0600

    [#85239860] Updated test cases, updated application dummy config, updated tavis config to remove timezone config.

--- spec/features/participant/activity_tracker_spec.rb
+++ spec/features/participant/activity_tracker_spec.rb
@@ -5,7 +5,7 @@ feature "activity tracker", type: :feature do
     :participants, :"bit_core/slideshows", :"bit_core/slides",
     :"bit_core/tools", :users, :arms,
     :"bit_core/content_modules", :"bit_core/content_providers",
-    :activity_types, :activities, :groups, :memberships, :tasks, :task_status
+    :activity_types, :activities, :groups, :memberships, :tasks, :task_status, :awake_periods
   )
 
   context "Participant1 is logged in" do
@@ -16,7 +16,11 @@ feature "activity tracker", type: :feature do
       visit "/navigator/contexts/DO"
     end
 
-    it "displays most recent unaccounted activity" do
+    def choose_rating(element_id, value)
+      find("##{ element_id } select").find(:xpath, "option[#{(value + 1)}]").select_option
+    end
+
+    it "displays most recent unaccounted activity" :js do
 
       within ".container .left.list-group" do
         click_on "#1 Awareness"
@@ -53,65 +57,50 @@ feature "activity tracker", type: :feature do
 
       click_on "Next"
       expect(page).to have_content "Your Activities"
-    end
 
-    it "implements #1 Awareness", :js do
-      page.find(".container .left.list-group .list-group-item", text: "#1 Awareness").trigger("click")
+      click_on "#1 Awareness"
+      expect(page).to have_content "This is just the beginning..."
 
-      expect(page).to have_text(bit_core_slides(:do_awareness_intro1).body)
-      find(".btn", text: "Next").trigger("click")
+      click_on "Next"
+      expect(page).to have_content "OK, let's talk about yesterday."
 
-      expect(page).to have_text("OK, let's talk about yesterday.")
-
-      yesterday_str = Date.yesterday.strftime("%a")
-      select "#{ yesterday_str } 12 AM",
-             from: "About what time did you wake up? It's okay if this isn't " \
-                   "exact."
-      select "#{ yesterday_str } 4 AM",
-             from: "About what time did you go to sleep? This doesn't need " \
-                   "to be exact either."
+      today = Date.today
+      select today.strftime("%a") + " 1 AM", from: "awake_period_start_time"
+      select today.strftime("%a") + " 5 AM", from: "awake_period_end_time"
       click_on "Create"
 
       expect(page).to have_text("Review Your Day")
 
-      find("#activity_type_0").trigger("click")
-      fill_in("activity_type_0", with: "ate cheeseburgers")
+      fill_in "activity_type_0", with: "ate cheeseburgers"
       choose_rating("pleasure_0", 9)
       choose_rating("accomplishment_0", 4)
 
-      find("#activity_type_1").trigger("click")
-      fill_in("activity_type_1", with: "ate bad cheeseburgers")
+      fill_in "activity_type_1", with: "ate bad cheeseburgers"
       choose_rating("pleasure_1", 0)
       choose_rating("accomplishment_1", 0)
 
       find("#copy_2").trigger("click")
 
-      find('button#submit_activities[type="submit"]').click
-
-      expect(page).to have_css("div.has-error")
-
-      find("#activity_type_3").trigger("click")
-      fill_in("activity_type_3", with: "ate bad cheeseburgers")
+      fill_in "activity_type_3", with: "ate bad cheeseburgers"
       choose_rating("pleasure_3", 0)
       choose_rating("accomplishment_3", 1)
 
-      find('button#submit_activities[type="submit"]').click
-
+      click_on "Next"
       expect(page).to have_text("ate cheeseburgers")
       expect(page).to have_text("ate bad cheeseburgers", count: 3)
 
-      find(".btn", text: "Next").trigger("click")
+      click_on "Next"
 
       expect(page).to have_text("Things you found fun.")
       expect(page).to have_text("ate cheeseburgers")
 
-      find(".btn", text: "Next").trigger("click")
+      click_on "Next"
 
       expect(page).to have_text("Things that make you feel like you've accomplished something.")
       expect(page).not_to have_text("ate cheeseburgers")
 
-      find(".btn", text: "Next").trigger("click")
-      expect(page).to have_text("#2 Planning")
+      click_on "Next"
+      expect(page).to have_text("#1 Awareness")
     end
 
     it "implements #2 Planning", :js do
@@ -124,8 +113,9 @@ feature "activity tracker", type: :feature do
       expect(page).to have_text("We want you to plan one fun thing")
 
       find("input[value='Loving'][type='radio']").click
-      find("input#future_date_picker_0").click
-      find("#ui-datepicker-div .ui-datepicker-today a").click
+      tomorrow = Date.today + 1
+      find(".fa.fa-calendar").click
+      click_on tomorrow.strftime("%e")
       choose_rating "pleasure_0", 10
       choose_rating "accomplishment_0", 10
       click_on "Next"
@@ -135,6 +125,9 @@ feature "activity tracker", type: :feature do
       expect(page).to have_text("Now, plan something that gives you a sense of accomplishment.")
 
       fill_in "activity_activity_type_new_title", with: "Parkour"
+      tomorrow = Date.today + 1
+      find(".fa.fa-calendar").click
+      click_on tomorrow.strftime("%e")
       click_on "Next"
 
       expect(page.body).to have_text("OK... the most important thing")
@@ -167,43 +160,6 @@ feature "activity tracker", type: :feature do
 
       expect(page).to have_text("You said you were going to")
       expect(page).to have_text("Loving")
-    end
-
-    it "creates new accomplishable and/or pleasurable activities via radio", :js do
-      click_on "Add a New Activity"
-
-      expect(page).to have_text("Choose one")
-
-      find("input[value='Loving'][type='radio']").click
-      choose_rating "pleasure_0", 10
-      choose_rating "accomplishment_0", 10
-      click_on "Next"
-
-      expect(page).to have_text("Activity saved")
-
-      within "#Upcoming_Activities table.table" do
-        expect(page).to have_text "Loving"
-        expect(page).to have_text((Time.current + 1.hour).to_s(:date_time_with_meridian))
-        expect(page).to have_text "Really fun (10)"
-        expect(page).to have_text "High Importance (10)"
-      end
-    end
-
-    it "creates new accomplishable and/or pleasurable activities input text field", :js do
-      click_on "Add a New Activity"
-      fill_in "Or add another", with: "Eating!"
-      choose_rating "pleasure_0", 10
-      choose_rating "accomplishment_0", 10
-      click_on "Next"
-
-      expect(page).to have_text("Activity saved")
-
-      within "#Upcoming_Activities table.table" do
-        expect(page).to have_text "Eating!"
-        expect(page).to have_text((Time.current + 1.hour).to_s(:date_time_with_meridian))
-        expect(page).to have_text "Really fun (10)"
-        expect(page).to have_text "High Importance (10)"
-      end
     end
 
     it "edits activities", :js do
@@ -240,7 +196,7 @@ feature "activity tracker", type: :feature do
     let(:activity) { activities(:p2_activity_1_hr_ago) }
 
     before do
-      t = Time.local(2015,"jan",15,10,30,0)
+      t = Time.now - 268.hours
       Timecop.travel(t)
       sign_in_participant participant
       visit "/navigator/modules/#{bit_core_content_modules(:do_your_activities_viz).id}"
@@ -262,7 +218,7 @@ feature "activity tracker", type: :feature do
     let(:activity) { activities(:p2_activity_1_hr_ago) }
 
     before do
-      t = Time.local(2015,"jan",15,17,1,0)
+      t = Time.now - 288.hours
       Timecop.travel(t)
       sign_in_participant participant
       visit "/navigator/modules/#{bit_core_content_modules(:do_your_activities_viz).id}"
@@ -273,7 +229,8 @@ feature "activity tracker", type: :feature do
     end
 
     it "displays daily activity averages" do
-      expect(page).to have_text "Daily Averages for Jan 15, 2015"
+      today = Date.today
+      expect(page).to have_text "Daily Averages for " + today.strftime("%b %d, %Y")
       expect(page).to have_text "Mood: No Recordings"
       expect(page).to have_text "Positive Emotions: No Recordings"
       expect(page).to have_text "Negative Emotions: No Recordings"
@@ -289,10 +246,12 @@ feature "activity tracker", type: :feature do
     end
 
     it "navigates to today when 'Today' button is clicked" do
-      visit "/navigator/modules/#{bit_core_content_modules(:do_your_activities_viz).id}?date=14/1/2015"
-
-      expect(page).to have_text "Daily Averages for Jan 14, 2015"
-      expect(page).to_not have_text "Daily Averages for Jan 15, 2015"
+      today = Date.today
+      yesterday = Date.today - 1
+      visit "/navigator/modules/#{bit_core_content_modules(:do_your_activities_viz).id}"
+      click_on "Previous Day"
+      expect(page).to have_text "Daily Averages for " + yesterday.strftime("%b %d, %Y")
+      expect(page).to_not have_text "Daily Averages for" + today.strftime("%b %d, %Y")
 
       click_on "Today"
 
@@ -300,16 +259,14 @@ feature "activity tracker", type: :feature do
     end
 
     it "displays a list of activities and activity details" do
-      expect(page).to have_text "2 pm - 3 pm: Eating breakfast"
+      expect(page).to have_text "Eating breakfast"
       expect(page).to have_text "Accomplishment: 9 · Pleasure: 9"
-
       expect(page).to have_text "Accomplishment  Pleasure"
       expect(page).to have_text "Predicted High Importance: 10 Really fun: 10"
       expect(page).to have_text "Actual  High Importance: 9  Really fun: 9"
       expect(page).to have_text "Difference  1  1"
-      expect(page).to have_text "3 pm - 4 pm: Working"
+      expect(page).to have_text "Working"
       expect(page).to have_text "Accomplishment: 2 · Pleasure: 2"
-      expect(page).to have_text "4 pm - 5 pm: Working"
     end
 
     it "title is displayed when data is selected", :js do
@@ -327,25 +284,24 @@ feature "activity tracker", type: :feature do
     end
 
     it "allows for the updating of a past activity", :js do
-      expect(page).to have_text "4 pm - 5 pm: Working"
-
       expect(page).to_not have_text "Predicted Average Importance: 6 Kind of fun: 5"
       expect(page).to_not have_text "Actual  Not answered: Not answered:"
       expect(page).to_not have_text "Difference N/A N/A"
 
-      click_on "4 pm - 5 pm: Working"
+      page.all("a", text: "Working")[1].click
 
       expect(page).to have_text "Predicted Average Importance: 6 Kind of fun: 5"
       expect(page).to have_text "Actual  Not answered: Not answered:"
       expect(page).to have_text "Difference N/A N/A"
 
       within "form#edit_activity_#{activity.id}" do
-        expect(page).to_not have_button "Update"
-        expect(page).to_not have_button "Cancel"
+        page.all(".btn.btn-primary.not-displayed", visible: "false")
+        page.all(".btn.btn-default.show-table.not-displayed", visible: "false")
 
         click_on "Edit"
 
-        expect(page).to have_button "Cancel"
+        page.all(".btn.btn-primary.not-displayed", visible: "true")
+        page.all(".btn.btn-default.show-table.not-displayed", visible: "true")
 
         select "1", from: "Actual accomplishment intensity"
         select "8", from: "Actual pleasure intensity"
@@ -356,7 +312,7 @@ feature "activity tracker", type: :feature do
       expect(page).to_not have_button "Cancel"
       expect(page).to have_text "Accomplishment: 1 · Pleasure: 8"
 
-      click_on "4 pm - 5 pm: Working"
+      page.all("a", text: "Working")[1].click
 
       expect(page).to have_text "Predicted  Average Importance: 6 Kind of fun: 5"
       expect(page).to have_text "Actual  Low Importance: 1 Really fun: 8"
@@ -364,19 +320,12 @@ feature "activity tracker", type: :feature do
     end
 
     it "allows for the paginating to the previous day's activities" do
-      expect(page).to_not have_text "3 pm - 2 pm: Working"
-      expect(page).to_not have_text "Accomplishment: 8 · Pleasure: 9"
-      expect(page).to_not have_text "Predicted Low Importance: 1 Not Fun: 2"
-      expect(page).to_not have_text "Actual  High Importance: 8  Really fun: 9"
-      expect(page).to_not have_text "Difference  7 7"
+      expect(page).to_not have_text "Jogging"
 
       click_on "Previous Day"
 
-      expect(page).to have_text "9 pm - 8 pm: Working"
-      expect(page).to have_text "Accomplishment: 8 · Pleasure: 9"
-      expect(page).to have_text "Predicted Low Importance: 1 Not Fun: 2"
-      expect(page).to have_text "Actual  High Importance: 8  Really fun: 9"
-      expect(page).to have_text "Difference  7 7"
+      expect(page).to have_text "Jogging"
+      expect(page).to have_text "Accomplishment: 6 · Pleasure: 7"
     end
 
     it "allows for the paginating to the next day's activities" do
@@ -396,7 +345,7 @@ feature "activity tracker", type: :feature do
     let(:participant) { participants(:traveling_participant2) }
 
     before do
-      t = Time.local(2015,"jan",15,1,00,0)
+      t = Time.now - 283.hours
       Timecop.travel(t)
       sign_in_participant participant
       visit "/navigator/modules/#{bit_core_content_modules(:do_your_activities_viz).id}"
@@ -459,10 +408,10 @@ feature "activity tracker", type: :feature do
     let(:participant) { participants(:traveling_participant3) }
 
     before do
-      t = Time.local(2015,"jan",17,1,00,0)
+      t = Time.now - 240.hours
       Timecop.travel(t)
       sign_in_participant participant
-      visit "/navigator/modules/#{bit_core_content_modules(:do_your_activities_viz).id}?date=16/01/2015"
+      visit "/navigator/modules/#{bit_core_content_modules(:do_your_activities_viz).id}"
     end
 
     after do
