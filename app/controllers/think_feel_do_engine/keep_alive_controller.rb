e4ee12435f5067cf54ce969abd8e53e0ba9fee39
Eric Schlange <eric.schlange@gmail.com>
2015-02-11 13:55:52 -0600

    [Finished #88066350] Updated keep alive controller to include users. Added new specs for controller unit testing.

--- app/controllers/think_feel_do_engine/keep_alive_controller.rb
+++ app/controllers/think_feel_do_engine/keep_alive_controller.rb
@@ -1,11 +1,27 @@
 module ThinkFeelDoEngine
   # Authenticate user
   class KeepAliveController < ApplicationController
-    before_action :authenticate_participant!
+    before_action :authenticated_participant_or_user?
 
     # GET /arms
     def index
-      render nothing: true, status: :ok
+      if @authenticated
+        render nothing: true, status: :ok
+      else
+        render nothing: true, status: :unauthorized
+      end
+    end
+
+    private
+
+    def authenticated_participant_or_user?
+      if current_user
+        authenticate_user!
+        @authenticated = true
+      elsif current_participant
+        authenticate_participant!
+        @authenticated = true
+      end
     end
   end
 end
