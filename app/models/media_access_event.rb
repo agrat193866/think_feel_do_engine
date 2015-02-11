8ec45349316dc9fc9674ade2dd0b3d3cfad453a5
Tag0Mag0 <jem.hilton@gmail.com>
2015-02-11 13:20:00 -0600

    updated participant dash table for media access events
    [#87841636]

--- app/models/media_access_event.rb
+++ app/models/media_access_event.rb
@@ -10,4 +10,11 @@ class MediaAccessEvent < ActiveRecord::Base
             :participant_id,
             :bit_core_slide_id,
             presence: :true
+
+  def task_release_day
+    Task.where(
+      bit_core_content_module_id: self.slide.slideshow.content_provider.content_module.id,
+      group_id: self.participant.active_group.id
+    ).first.release_day
+  end
 end
