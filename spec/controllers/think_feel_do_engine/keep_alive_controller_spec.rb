11b52ed6323b3f2617a9070f65b477b31c0d825e
Eric Schlange <eric.schlange@gmail.com>
2015-02-11 13:55:52 -0600

    [Finished #88066350] Updated keep alive controller to include users. Added new specs for controller unit testing.

--- spec/controllers/think_feel_do_engine/keep_alive_controller_spec.rb
+++ spec/controllers/think_feel_do_engine/keep_alive_controller_spec.rb
@@ -2,8 +2,10 @@ require "rails_helper"
 
 module ThinkFeelDoEngine
   describe KeepAliveController, type: :controller do
-    describe "Participant GET" do
-      context "for authenticated requests" do
+    describe "GET" do
+      fixtures :all
+
+      context "for authenticated participants" do
         before do
           sign_in_participant
           get :index, use_route: :think_feel_do_engine
@@ -15,13 +17,37 @@ module ThinkFeelDoEngine
         end
       end
 
-      context "for unauthenticated requests" do
+      context "for unauthenticated participants" do
         before do
           get :index, use_route: :think_feel_do_engine
         end
 
         it "should render nothing with a Forbidden response" do
-          expect(response.body).to have_content("redirected")
+          expect(response.body).to be_blank
+          expect(response.status).to eq(401)
+        end
+      end
+
+      context "for authenticated users" do
+        before do
+          sign_in_user users(:clinician1)
+          get :index, use_route: :think_feel_do_engine
+        end
+
+        it "should render nothing with an OK response" do
+          expect(response.body).to be_blank
+          expect(response.status).to eq(200)
+        end
+      end
+
+      context "for unauthenticated users" do
+        before do
+          get :index, use_route: :think_feel_do_engine
+        end
+
+        it "should render nothing with a Forbidden response" do
+          expect(response.body).to be_blank
+          expect(response.status).to eq(401)
         end
       end
     end
