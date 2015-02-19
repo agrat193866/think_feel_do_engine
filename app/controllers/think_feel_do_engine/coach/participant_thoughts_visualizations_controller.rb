918c097a805b038728fd159dc621d83b16c32efb
Eric Carty-Fickes <ericcf@northwestern.edu>
2015-02-19 15:33:19 -0600

    enable breadcrumbs on the thought viz
    [#88710572]

--- app/controllers/think_feel_do_engine/coach/participant_thoughts_visualizations_controller.rb
+++ app/controllers/think_feel_do_engine/coach/participant_thoughts_visualizations_controller.rb
@@ -5,8 +5,8 @@ module ThinkFeelDoEngine
       before_action :authenticate_user!
 
       def show
-        participant = Participant.find(params[:participant_id])
-        thoughts = participant.thoughts.harmful
+        @participant = Participant.find(params[:participant_id])
+        thoughts = @participant.thoughts.harmful
         render "think_feel_do_engine/thoughts/distortion_viz",
                locals: { thoughts: thoughts, link_to_view: nil }
       end
