ecbb1629f0055143d1614db3755313e7b24927b1
Eric Carty-Fickes <ericcf@northwestern.edu>
2015-02-23 14:33:21 -0600

    import module page view and module session reports
    [#88533116] [#88479844]

---
+++ app/models/think_feel_do_engine/reports/tool_module.rb
@@ -0,0 +1,36 @@
+module ThinkFeelDoEngine
+  module Reports
+    module ToolModule
+      URL_ROOT_RE = /^[^\/]*\/\/[^\/]+/
+
+      def self.included(klass)
+        class << klass
+          def module_entries_map
+            modules = BitCore::ContentModule.where(type: nil).map do |m|
+              path = url_helpers.navigator_location_path(module_id: m.id)
+
+              ["#{ path }", m.id]
+            end
+
+            Hash[modules]
+          end
+
+          # Returns a hash mapping path to Tool Module.
+          def modules_map
+            modules = BitCore::ContentModule.where(type: nil).map do |m|
+              path = url_helpers.navigator_location_path(module_id: m.id)
+
+              ["#{ path }", m]
+            end
+
+            Hash[modules]
+          end
+
+          def url_helpers
+            ThinkFeelDoEngine::Engine.routes.url_helpers
+          end
+        end
+      end
+    end
+  end
+end
