25fcf21360639c639efb22c83727f2ece408bbc4
Eric Carty-Fickes <ericcf@northwestern.edu>
2015-03-03 14:15:14 -0600

    fix inaccurately reported Module Session report
    [#88479844]

--- spec/models/think_feel_do_engine/reports/module_session_spec.rb
+++ spec/models/think_feel_do_engine/reports/module_session_spec.rb
@@ -5,12 +5,16 @@ module ThinkFeelDoEngine
     RSpec.describe ModuleSession do
       fixtures :all
 
+      def data
+        @data ||= ModuleSession.all
+      end
+
       describe ".all" do
         context "when no modules were viewed" do
           it "returns an empty array" do
             EventCapture::Event.destroy_all
 
-            expect(ModuleSession.all).to be_empty
+            expect(data).to be_empty
           end
         end
 
@@ -18,8 +22,6 @@ module ThinkFeelDoEngine
           it "returns accurate summaries" do
             first_render_event = event_capture_events(:event_capture_events_015)
             last_render_event = event_capture_events(:event_capture_events_015)
-            data = ModuleSession.all
-
             expect(data.count).to eq 7
             expect(data).to include(
               participant_id: "TFD-1111",
@@ -29,8 +31,42 @@ module ThinkFeelDoEngine
                              "are the exact sentences that go through your " \
                              "head."],
               module_selected_at: first_render_event.emitted_at.iso8601,
-              last_page_number_opened: 1,
               last_page_opened_at: last_render_event.emitted_at.iso8601
+            )
+          end
+        end
+
+        context "when there was a gap in page views" do
+          def click!(emitted_at:, url:)
+            EventCapture::Event.create!(
+              emitted_at: emitted_at,
+              recorded_at: emitted_at,
+              payload: { currentUrl: url, headers: %w( a b c ) },
+              kind: "click",
+              participant_id: participants(:participant1).id
+            )
+          end
+
+          it "returns accurate summaries" do
+            EventCapture::Event.destroy_all
+            now = Time.current
+            module_id = bit_core_content_modules(:do_awareness).id
+            click!(emitted_at: now - 10.minutes,
+                   url: "/navigator/modules/#{ module_id }")
+            click!(emitted_at: now - 9.minutes,
+                   url: "/navigator/modules/#{ module_id }/providers/123/1")
+            click!(emitted_at: now - 8.minutes,
+                   url: "/navigator/modules/#{ module_id }/providers/123/2")
+            click!(emitted_at: now - 1.minute,
+                   url: "/navigator/modules/#{ module_id }/providers/123/3")
+
+            expect(data.count).to eq 1
+            expect(data).to include(
+              participant_id: "TFD-1111",
+              module_id: module_id,
+              page_headers: ["a", "b", "c"],
+              module_selected_at: (now - 10.minutes).utc.iso8601,
+              last_page_opened_at: (now - 8.minutes).utc.iso8601
             )
           end
         end
