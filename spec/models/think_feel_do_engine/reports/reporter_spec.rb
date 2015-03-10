0e56fba5e3da187bbc53c93a101614921b005e60
Tag0Mag0 <jem.hilton@gmail.com>
2015-03-10 14:04:54 -0500

    Add spec for reporter file path generator
    [#89341398]

--- spec/models/think_feel_do_engine/reports/reporter_spec.rb
+++ spec/models/think_feel_do_engine/reports/reporter_spec.rb
@@ -3,14 +3,22 @@ require "rails_helper"
 module ThinkFeelDoEngine
   module Reports
     RSpec.describe Reporter do
+      let(:collector) do
+        double(name: "collector",
+               columns: %w( a b ),
+               all: [{ a: 1, b: 2 }, { a: "x", b: "y" }]
+              )
+      end
+
       describe "#format_csv" do
         it "produces proper input for csv" do
-          collector = double("collector",
-                             name: "collector",
-                             columns: %w( a b ),
-                             all: [{ a: 1, b: 2 }, { a: "x", b: "y" }])
-
           expect(Reporter.new(collector).to_csv).to eq([{ a: 1, b: 2 }, { a: "x", b: "y" }])
+        end
+      end
+
+      describe ".file_path" do
+        it "generates the proper file path" do
+          expect(Reporter.file_path(collector.name)).to eq(Rails.root.join("reports/collector.csv"))
         end
       end
     end
