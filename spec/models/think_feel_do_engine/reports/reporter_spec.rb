fe0fe400139be99a27661b80f3b84bea543b5060
gwensmu <gwen.smuda@gmail.com>
2015-03-11 15:17:18 -0500

    Reporter sanitizes the file path more strictly
    * Only allow [a-z]+ charecters
    * Discard the rest
    * Add a spec to test cleanliness
    
    [#90118312]

--- spec/models/think_feel_do_engine/reports/reporter_spec.rb
+++ spec/models/think_feel_do_engine/reports/reporter_spec.rb
@@ -4,10 +4,18 @@ module ThinkFeelDoEngine
   module Reports
     RSpec.describe Reporter do
       let(:collector) do
-        double(name: "collector",
-               columns: %w( a b ),
-               all: [{ a: 1, b: 2 }, { a: "x", b: "y" }]
-              )
+        double(
+          name: "collector",
+          columns: %w( a b ),
+          all: [{ a: 1, b: 2 }, { a: "x", b: "y" }]
+        )
+      end
+      let(:messy_collector) do
+        double(
+          name: "collector23%",
+          columns: %w( a b ),
+          all: [{ a: 1, b: 2 }, { a: "x", b: "y" }]
+        )
       end
 
       describe "#format_csv" do
@@ -19,6 +27,10 @@ module ThinkFeelDoEngine
       describe ".file_path" do
         it "generates the proper file path" do
           expect(Reporter.file_path(collector.name)).to eq(Rails.root.join("reports/collector.csv"))
+        end
+
+        it "sanitizes the file path to only include letters" do
+          expect(Reporter.file_path(messy_collector.name)).to eq(Rails.root.join("reports/collector.csv"))
         end
       end
     end
