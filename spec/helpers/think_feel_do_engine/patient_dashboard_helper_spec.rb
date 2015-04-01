395b2c606c431f485844e1cd42c94af8e1728276
Eric Schlange <eric.schlange@gmail.com>
2015-04-01 13:15:23 -0500

    Update planned activities counts.
    * added helper methods for planned activity counts.
    * added specs to test validity of planned activity counts.
    
    [Finished #91421272]

--- spec/helpers/think_feel_do_engine/patient_dashboard_helper_spec.rb
+++ spec/helpers/think_feel_do_engine/patient_dashboard_helper_spec.rb
@@ -3,6 +3,9 @@ require "rails_helper"
 module ThinkFeelDoEngine
   module Coach
     RSpec.describe PatientDashboardHelper, type: :helper do
+      let(:activities) { double("activities") }
+      let(:participant) { double("participant", activities: activities) }
+
       describe "#breadcrumbs" do
         it "returns nil for the wrong controller" do
           allow(helper).to receive(:controller_name) { "baz" }
@@ -21,6 +24,45 @@ module ThinkFeelDoEngine
             .with(:breadcrumbs, /Patient Dashboard/)
 
           helper.breadcrumbs
+        end
+      end
+
+      describe "#activities_planned_today" do
+        it "should return a sum of all planned activities for today" do
+          expect(participant).to receive(:activities).exactly(3).times { activities }
+          expect(activities)
+            .to receive_message_chain(:planned, :updated_for_day, :count) { 1 }
+          expect(activities)
+            .to receive_message_chain(:reviewed_and_complete, :updated_for_day, :count) { 1 }
+          expect(activities)
+            .to receive_message_chain(:reviewed_and_incomplete, :updated_for_day, :count) { 1 }
+          expect(activities_planned_today(participant)).to eq(3)
+        end
+      end
+
+      describe "#activities_planned_7_day" do
+        it "should return a sum of planned activities for the last 7 days" do
+          expect(participant).to receive(:activities).exactly(3).times { activities }
+          expect(activities)
+            .to receive_message_chain(:planned, :last_seven_days, :count) { 1 }
+          expect(activities)
+            .to receive_message_chain(:reviewed_and_complete, :last_seven_days, :count) { 1 }
+          expect(activities)
+            .to receive_message_chain(:reviewed_and_incomplete, :last_seven_days, :count) { 1 }
+          expect(activities_planned_7_day(participant)).to eq(3)
+        end
+      end
+
+      describe "#activities_planned_total" do
+        it "should return a sum of all planned activities for all time" do
+          expect(participant).to receive(:activities).exactly(3).times { activities }
+          expect(activities)
+            .to receive_message_chain(:planned, :count) { 1 }
+          expect(activities)
+            .to receive_message_chain(:reviewed_and_complete, :count) { 1 }
+          expect(activities)
+            .to receive_message_chain(:reviewed_and_incomplete, :count) { 1 }
+          expect(activities_planned_total(participant)).to eq(3)
         end
       end
     end
