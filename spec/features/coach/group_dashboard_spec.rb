e58d254ed84a793a8dd4079ca1d8378cf904696d
Eric Carty-Fickes <ericcf@northwestern.edu>
2015-04-06 14:51:07 -0500

    replace actual config mutations with method stubs
    * this is an attempt to fix the sporadically failing spec at
      features/coach/patient_dashboard_spec.rb:452

--- spec/features/coach/group_dashboard_spec.rb
+++ spec/features/coach/group_dashboard_spec.rb
@@ -3,21 +3,15 @@ require "rails_helper"
 feature "group dashboard", type: :feature do
   fixtures :all
 
-  after :each do
-    allow(Rails.application.config).to receive(:include_social_features)
-      .and_return(false)
-  end
-
   describe "Logged in as a clinician" do
     let(:clinician) { users(:clinician1) }
     let(:group1) { groups(:group1) }
 
     context "Coach views table with many patients" do
       before do
-        Rails.application.configure do
-          config.study_length_in_weeks = 8
-          config.include_social_features = true
-        end
+        allow(Rails.application.config).to receive(:study_length_in_weeks) { 8 }
+        allow(Rails.application.config)
+          .to receive(:include_social_features) { true }
         sign_in_user clinician
         visit "/coach/groups/#{group1.id}/group_dashboard"
       end
