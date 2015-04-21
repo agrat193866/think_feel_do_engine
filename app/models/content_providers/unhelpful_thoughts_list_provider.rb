c8af8c9de9bc4689bd8bbd48a4daf8ee2e2e2b4e
Eric Carty-Fickes <ericcf@dhcp-10-101-87-245.wireless.northwestern.private>
2015-04-21 13:12:14 -0500

    add a kludgy fix for the thought add links
    [#92725878]

--- app/models/content_providers/unhelpful_thoughts_list_provider.rb
+++ app/models/content_providers/unhelpful_thoughts_list_provider.rb
@@ -38,18 +38,29 @@ module ContentProviders
       if @thoughts.count > 0
         "We're going to ask you to challenge each thought:"
       else
-        module_1 = BitCore::ContentModule.find_by_title("#1 Identifying")
-        module_2 = BitCore::ContentModule.find_by_title("Add a New Thought")
+        title_1, title_2 = "#1 Identifying", "Add a New Harmful Thought"
+        module_1 = BitCore::ContentModule.find_by_title(title_1)
+        module_2 = BitCore::ContentModule.find_by_title(title_2)
 
-        "You can log more thoughts in " \
-        "#{ @view.link_to(
-          'Think #1 Identifying',
-          @view.navigator_location_path(module_id: module_1.id),
-          style: 'font-size: larger;') } or use " \
-        "#{ @view.link_to(
-          'Add a New Thought',
-          @view.navigator_location_path(module_id: module_2.id),
-          style: 'font-size: larger;') } to do multiple steps at once."
+        if module_1 && module_2
+          "You can log more thoughts in " \
+          "#{ @view.link_to(
+            title_1,
+            @view.navigator_location_path(module_id: module_1.id),
+            style: 'font-size: larger;') } or use " \
+          "#{ @view.link_to(
+            title_2,
+            @view.navigator_location_path(module_id: module_2.id),
+            style: 'font-size: larger;') } to do multiple steps at once."
+        else
+          if defined? Raven
+            event = Raven::Event.new(message: "one of the thought modules " \
+                                              "couldn't be found via title")
+            Raven.send(event)
+          end
+
+          nil
+        end
       end
     end
   end
