18726c59870e867a35dccc8125d53fb8e1a960e3
Christopher Brenner <christopher.brenner1@northwestern.edu>
2015-05-13 11:12:12 -0500

    Add spec for seven day logins on patient dash

--- spec/features/coach/patient_dashboard_spec.rb
+++ spec/features/coach/patient_dashboard_spec.rb
@@ -237,6 +237,21 @@ feature "patient dashboard", type: :feature do
         end
       end
 
+      it "summarizes logins in last 7 days" do
+        sign_in_participant participant1
+        Timecop.travel(Time.zone.now + 2.days) do
+          sign_in_participant participant1
+          sign_in_user clinician
+          visit "/coach/groups/#{group1.id}/patient_dashboards/#{participant1.id}"
+          expect(page).to have_content "Logins in the last seven days: 2"
+          Timecop.travel(Time.zone.now + 6.days) do
+            sign_in_user clinician
+            visit "/coach/groups/#{group1.id}/patient_dashboards/#{participant1.id}"
+            expect(page).to have_content "Logins in the last seven days: 1"
+          end
+        end
+      end
+
       it "summarizes learning when not completed learning", :js do
         Timecop.travel(DateTime.now.beginning_of_minute) do
           visit "/coach/groups/#{ group1.id }/patient_dashboards/#{ participant1.id }"
