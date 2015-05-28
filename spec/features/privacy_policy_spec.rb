f5fe4ab204639c76254cc550cb86ecb870d5fae5
Eric Schlange <eric.schlange@gmail.com>
2015-05-28 08:50:25 -0500

    Show navbar on privacy policy page for authenticated participants.
    * Updated privacy policy controller.
    * Added feature spec.
    * Cleaned up some activity_tracker javascript.
    
    [#95492972]

--- spec/features/privacy_policy_spec.rb
+++ spec/features/privacy_policy_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 feature "Privacy Policy", :js, type: :feature do
+  fixtures :all
+
   it "not obscured in the footer" do
     visit "/participants/sign_in"
     click_on "Privacy Policy"
@@ -12,5 +14,22 @@ feature "Privacy Policy", :js, type: :feature do
     visit "/privacy_policy"
 
     expect(page).to have_text("Privacy Policy")
+  end
+
+  it "should not show navbar for unauthenticated participants" do
+    expect(page).to_not have_css("#navbar-collapse")
+  end
+
+  context "participant is logged in" do
+    let(:participant1) { participants(:participant1) }
+
+    before :each do
+      sign_in_participant participant1
+      visit "/privacy_policy"
+    end
+
+    it "should show navbar for authenticated participants" do
+      expect(page).to have_css("#navbar-collapse")
+    end
   end
 end
