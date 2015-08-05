8eb22c13c878906b0dda9a5294aab79bd6410301
Eric Carty-Fickes <ericcf@northwestern.edu>
2015-08-05 12:44:27 -0500

    improve readability of Membership specs

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -13,12 +13,13 @@ describe Membership do
   let(:membership1) { memberships(:membership1) }
 
   describe "validations" do
-    it "does not allow more than one active membership for a participant" do
+    it "prohibits more than one active membership for a participant" do
       new_membership = Membership.new(
         group_id: group.id,
         participant_id: participants(:active_participant).id,
         start_date: Date.today,
         end_date: Date.tomorrow)
+
       expect(new_membership).not_to be_valid
     end
 
@@ -26,12 +27,12 @@ describe Membership do
       expect { memberships(:membership1).save! }.not_to raise_error
     end
 
-    it "doesn't allow changing the group_id" do
+    it "prohibits changing the group_id" do
       expect(memberships(:membership1).update(group: groups(:group2)))
         .to eq false
     end
 
-    it "does not allow is_complete=true and an end_date in the future" do
+    it "prohibits is_complete=true and an end_date in the future" do
       expect(
         Membership.new(end_date: Date.today + 3.days, is_complete: true)
         .tap(&:valid?)
@@ -39,24 +40,25 @@ describe Membership do
       ).not_to be_empty
     end
 
-    it "does not allow an end date to be set in the past" do
+    it "prohibits an end date to be set in the past" do
       expect(Membership.new(end_date: Date.yesterday).tap(&:valid?).errors[:end_date].length)
         .to be > 0
     end
 
     it "allows an end date in the past to exist" do
       existing_membership = Membership.first
+
       Timecop.travel existing_membership.end_date + 1.day do
         expect(existing_membership).to be_valid
       end
     end
 
-    it "does not allow a nil end date to be created" do
+    it "prohibits a nil end date to be created" do
       expect(Membership.new(end_date: nil).tap(&:valid?).errors[:end_date].length)
         .to be > 0
     end
 
-    it "does not allow a nil end date to be updated" do
+    it "prohibits a nil end date to be updated" do
       expect(Membership.first.update(end_date: nil)).to eq false
     end
   end
@@ -81,27 +83,30 @@ describe Membership do
     expect(new_task_ids).to eq task_ids
   end
 
-  it "assigns all recurring tasks linked to a group upon creation" do
-    task_ids = group_without_members2.tasks.map(&:id)
+  context "after creation" do
+    it "assigns all recurring tasks linked to a group" do
+      task_ids = group_without_members2.tasks.map(&:id)
 
-    expect(participant_wo_membership2.active_membership).to be_nil
+      expect(participant_wo_membership2.active_membership).to be_nil
 
-    Membership.create!(
-      participant_id: participant_wo_membership2.id,
-      group_id: group_without_members2.id,
-      start_date: Date.today,
-      end_date: Date.today + 4.days)
-    new_task_ids = participant_wo_membership2
-                   .reload
-                   .active_membership.tasks.map(&:id)
+      Membership.create!(
+        participant_id: participant_wo_membership2.id,
+        group_id: group_without_members2.id,
+        start_date: Date.today,
+        end_date: Date.today + 4.days)
+      new_task_ids = participant_wo_membership2
+                     .reload
+                     .active_membership.tasks.map(&:id)
 
-    expect(new_task_ids.uniq).to eq task_ids
-    expect(new_task_ids.length).to eq 1
+      expect(new_task_ids.uniq).to eq task_ids
+      expect(new_task_ids.length).to eq 1
+    end
   end
 
   describe "#withdraw" do
     it "sets the end_date in the past" do
       membership1.update!(end_date: Date.tomorrow)
+
       expect(membership1.withdraw).to be true
       expect(membership1.end_date).to be < Date.current
     end
@@ -110,18 +115,20 @@ describe Membership do
   describe "#discontinue" do
     it "sets the end_date in the past" do
       membership1.update!(end_date: Date.tomorrow)
+
       expect(membership1.discontinue).to be true
       expect(membership1.end_date).to be < Date.current
     end
 
     it "sets is_complete to true" do
       membership1.update!(is_complete: false)
+
       expect(membership1.discontinue).to be true
       expect(membership1.end_date).to be < Date.current
     end
   end
 
-  describe "membership state modification" do
+  describe "#flag_complete" do
     let(:start_date) { Date.parse("2012-01-02") }
     let(:end_date) { Date.today }
     let(:participant1) { participants(:participant1) }
@@ -147,104 +154,117 @@ describe Membership do
     let(:participant_wo_membership3) { participants(:participant_wo_membership3) }
     let(:participant_wo_membership4) { participants(:participant_wo_membership4) }
 
-    it ".active returns memberships that are completed" do
-      active_completed_membership = group.memberships.create(
-        start_date: Date.current,
-        end_date: Date.current + 1.day,
-        participant: participant_wo_membership4)
+    describe ".active" do
+      it ".active returns memberships that are completed" do
+        active_completed_membership = group.memberships.create(
+          start_date: Date.current,
+          end_date: Date.current + 1.day,
+          participant: participant_wo_membership4)
 
-      Timecop.travel Date.current + 2.days do
-        active_completed_membership.update(is_complete: true)
+        Timecop.travel Date.current + 2.days do
+          active_completed_membership.update(is_complete: true)
 
-        expect(Membership.active).to include active_completed_membership
+          expect(Membership.active).to include active_completed_membership
+        end
+      end
+
+      it "returns memberships that are started before/on today and finishing today/in the future" do
+        count = Membership.active.count
+        group.memberships.create(
+          start_date: Date.yesterday,
+          end_date: Date.tomorrow,
+          participant: participant_wo_membership1)
+        group.memberships.create(
+          start_date: Date.today.advance(days: -2),
+          end_date: Date.today.advance(days: -1),
+          participant: participant_wo_membership2)
+        group.memberships.create(
+          start_date: Date.today.advance(days: 1),
+          end_date: Date.today.advance(days: 2),
+          participant: participant_wo_membership3)
+
+        expect(Membership.active.count).to eq count + 1
       end
     end
 
-    it ".active returns memberships that are started before/on today and finishing today/in the future" do
-      count = Membership.active.count
-      group.memberships.create(
-        start_date: Date.yesterday,
-        end_date: Date.tomorrow,
-        participant: participant_wo_membership1)
-      group.memberships.create(
-        start_date: Date.today.advance(days: -2),
-        end_date: Date.today.advance(days: -1),
-        participant: participant_wo_membership2)
-      group.memberships.create(
-        start_date: Date.today.advance(days: 1),
-        end_date: Date.today.advance(days: 2),
-        participant: participant_wo_membership3)
+    describe ".inactive" do
+      it "returns memberships that are started after today or finishing in the past" do
+        membership_in_the_past = group.memberships.create(
+          start_date: Date.current,
+          end_date: Date.current + 1.day,
+          participant: participant_wo_membership1)
+        membership_in_the_future = group.memberships.create(
+          start_date: Date.current + 3.days,
+          end_date: Date.current + 4.days,
+          participant: participant_wo_membership2)
 
-      expect(Membership.active.count).to eq count + 1
-    end
-
-    it ".inactive returns memberships that are started after today or finishing in the past" do
-      membership_in_the_past = group.memberships.create(
-        start_date: Date.current,
-        end_date: Date.current + 1.day,
-        participant: participant_wo_membership1)
-      membership_in_the_future = group.memberships.create(
-        start_date: Date.current + 3.days,
-        end_date: Date.current + 4.days,
-        participant: participant_wo_membership2)
-
-      Timecop.travel Date.current - 2.days do
-        inactive_memberships = Membership.inactive
-        expect(inactive_memberships).to include membership_in_the_past
-        expect(inactive_memberships).to include membership_in_the_future
+        Timecop.travel Date.current - 2.days do
+          inactive_memberships = Membership.inactive
+          expect(inactive_memberships).to include membership_in_the_past
+          expect(inactive_memberships).to include membership_in_the_future
+        end
       end
     end
 
-    it ".activities_future_by_week returns a count of activities "\
-        " monitored for the first week" do
-      expect(membership.logins_by_week(1)).to eq(2)
+    describe ".activities_future_by_week" do
+      it "returns a count of activities monitored for the first week" do
+        expect(membership.logins_by_week(1)).to eq(2)
+      end
     end
 
-    it ".continuing returns memberships where `is_complete` is false" do
-      expect do
-        memberships(:membership_participant_complete)
-          .update(is_complete: false)
-      end.to change { Membership.continuing.count }.by(1)
+    describe ".continuing" do
+      it "returns memberships where `is_complete` is false" do
+        expect do
+          memberships(:membership_participant_complete)
+            .update(is_complete: false)
+        end.to change { Membership.continuing.count }.by(1)
+      end
     end
   end
 
   describe "coach dashboard helper methods" do
-    it ".lessons_read returns completed tasks that are lessons for a membership" do
-      count = memberships(:membership1).lessons_read.count
-      TaskStatus.create(
-        membership_id: membership1.id,
-        task_id: lesson_task.id,
-        completed_at: Date.today
-      )
+    describe ".lessons_read" do
+      it "returns completed tasks that are lessons for a membership" do
+        count = memberships(:membership1).lessons_read.count
+        TaskStatus.create(
+          membership_id: membership1.id,
+          task_id: lesson_task.id,
+          completed_at: Date.today
+        )
 
-      expect(membership1.lessons_read.count).to eq count + 1
+        expect(membership1.lessons_read.count).to eq count + 1
+      end
     end
 
-    it ".lessons_read_for_day(time) returns lessons completed on a given day" do
-      count = memberships(:membership1).lessons_read_for_day(Date.today).count
-      TaskStatus.create(
-        membership_id: membership1.id,
-        task_id: lesson_task.id,
-        completed_at: Date.today
-      )
+    describe ".lessons_read_for_day" do
+      it "returns lessons completed on a given day" do
+        count = memberships(:membership1).lessons_read_for_day(Date.today).count
+        TaskStatus.create(
+          membership_id: membership1.id,
+          task_id: lesson_task.id,
+          completed_at: Date.today
+        )
 
-      expect(membership1.lessons_read_for_day(Date.today).count).to eq count + 1
+        expect(membership1.lessons_read_for_day(Date.today).count).to eq count + 1
+      end
     end
 
-    it ".lessons_read_for_week returns lessons completed in last seven days" do
-      count = memberships(:membership1).lessons_read_for_week.count
-      TaskStatus.create(
-        membership_id: membership1.id,
-        task_id: lesson_task.id,
-        completed_at: (Date.today - 1.day)
-      )
-      TaskStatus.create(
-        membership_id: membership1.id,
-        task_id: lesson_task.id,
-        completed_at: (Date.today - 8.days)
-      )
+    describe ".lessons_read_for_week" do
+      it "returns lessons completed in last seven days" do
+        count = memberships(:membership1).lessons_read_for_week.count
+        TaskStatus.create(
+          membership_id: membership1.id,
+          task_id: lesson_task.id,
+          completed_at: (Date.today - 1.day)
+        )
+        TaskStatus.create(
+          membership_id: membership1.id,
+          task_id: lesson_task.id,
+          completed_at: (Date.today - 8.days)
+        )
 
-      expect(membership1.lessons_read_for_week.count).to eq count + 1
+        expect(membership1.lessons_read_for_week.count).to eq count + 1
+      end
     end
   end
 end
