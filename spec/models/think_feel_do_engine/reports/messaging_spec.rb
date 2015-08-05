722e2c92c3e21d65b5b8a2937c8da7f5d0124029
Eric Carty-Fickes <ericcf@northwestern.edu>
2015-08-05 16:04:43 -0500

    add Messaging reports
    [#97400334]

---
+++ spec/models/think_feel_do_engine/reports/messaging_spec.rb
@@ -0,0 +1,113 @@
+require "rails_helper"
+
+module ThinkFeelDoEngine
+  module Reports
+    RSpec.describe Messaging do
+      fixtures :all
+
+      describe ".all" do
+        let(:clinician1) { users(:clinician1) }
+        let(:participant1) { participants(:participant1) }
+
+        context "when there are no messages" do
+          it "returns an empty array" do
+            Message.destroy_all
+
+            expect(Messaging.all).to be_empty
+          end
+        end
+
+        context "when there is a message from Coach to Participant" do
+          it "reports it" do
+            Message.destroy_all
+            m = Message.create!(sender: clinician1,
+                                recipient: participant1,
+                                subject: "Foo",
+                                body: "Bar",
+                                sent_at: DateTime.now)
+
+            report_line = Messaging.all.first
+
+            expect(report_line[:message_id]).to eq m.id
+            expect(report_line[:study_id]).to eq participant1.study_id
+            expect(report_line[:therapist_id]).to eq clinician1.email
+            expect(report_line[:sender_id]).to eq Messaging::COACH
+            expect(report_line[:sender]).to eq clinician1.email
+            expect(report_line[:recipient]).to eq participant1.study_id
+            expect(report_line[:start_dt]).to eq participant1.active_membership.start_date
+            expect(report_line[:treatment_week]).to eq 1
+            expect(report_line[:day]).to eq 0
+            expect(report_line[:event_date]).to eq m.sent_at.to_date.to_s(:db)
+            expect(report_line[:event_time]).to eq m.sent_at.utc.iso8601
+            expect(report_line[:is_message_opened]).to eq false
+            expect(report_line[:message_opened_at]).to be_nil
+            expect(report_line[:message_subject]).to eq "Foo"
+            expect(report_line[:message_content]).to eq "Bar"
+          end
+        end
+
+        context "when there is a message from Participant to Coach" do
+          it "reports it" do
+            Message.destroy_all
+            m = Message.create!(sender: participant1,
+                                recipient: clinician1,
+                                subject: "Foo",
+                                body: "Bar",
+                                sent_at: DateTime.now)
+
+            report_line = Messaging.all.first
+
+            expect(report_line[:message_id]).to eq m.id
+            expect(report_line[:study_id]).to eq participant1.study_id
+            expect(report_line[:therapist_id]).to eq clinician1.email
+            expect(report_line[:sender_id]).to eq Messaging::PARTICIPANT
+            expect(report_line[:sender]).to eq participant1.study_id
+            expect(report_line[:recipient]).to eq clinician1.email
+            expect(report_line[:start_dt]).to eq participant1.active_membership.start_date
+            expect(report_line[:treatment_week]).to eq 1
+            expect(report_line[:day]).to eq 0
+            expect(report_line[:event_date]).to eq m.sent_at.to_date.to_s(:db)
+            expect(report_line[:event_time]).to eq m.sent_at.utc.iso8601
+            expect(report_line[:is_message_opened]).to eq false
+            expect(report_line[:message_opened_at]).to be_nil
+            expect(report_line[:message_subject]).to eq "Foo"
+            expect(report_line[:message_content]).to eq "Bar"
+          end
+        end
+
+        context "when there is not an active Membership" do
+          it "reports nil start_dt, week and day values" do
+            Message.destroy_all
+            Membership.destroy_all
+            Message.create!(sender: participant1,
+                            recipient: clinician1,
+                            subject: "Foo",
+                            sent_at: DateTime.now)
+
+            report_line = Messaging.all.first
+
+            expect(report_line[:start_dt]).to be_nil
+            expect(report_line[:treatment_week]).to be_nil
+            expect(report_line[:day]).to be_nil
+          end
+        end
+
+        context "when the Message has been opened" do
+          it "reports it" do
+            Message.destroy_all
+            m = Message.create!(sender: participant1,
+                                recipient: clinician1,
+                                subject: "Foo",
+                                sent_at: DateTime.now)
+            m.delivered_messages.first.update!(is_read: true)
+
+            report_line = Messaging.all.first
+
+            expect(report_line[:is_message_opened]).to eq true
+            expect(report_line[:message_opened_at]).to eq m.updated_at.utc.iso8601
+          end
+        end
+      end
+    end
+  end
+end
