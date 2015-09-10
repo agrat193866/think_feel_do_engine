9748894344821a225e5b9b1df7d3323dc9fd916d
michaelwehrley <michaelwehrley@gmail.com>
2015-09-10 10:07:50 -0500

    Fix Media Access Bug
    * Membership was set to active, this was wrong - it should be
      the membership of the patients group - even if they are inactive
    * Presenter now sets `start` date on initialization.
    
    [#101934888]

--- app/helpers/think_feel_do_engine/presenters_helper.rb
+++
@@ -1,8 +0,0 @@
-module ThinkFeelDoEngine
-  # Used to present a presenter classes within a view.
-  module PresentersHelper
-    def present(object, klass)
-      yield klass.new(object)
-    end
-  end
-end
--- spec/presenters/think_feel_do_engine/media_access_event_presenter_spec.rb
+++ spec/presenters/think_feel_do_engine/media_access_event_presenter_spec.rb
@@ -11,38 +11,34 @@ module ThinkFeelDoEngine
         task_release_day: 3)
     end
 
-    def media_access_event(event = nil)
-      MediaAccessEventPresenter.new(event)
+    def media_access_event
+      MediaAccessEventPresenter.new(event: event, start_date: Date.today)
     end
 
     describe "#completed" do
       it "returns truthy if ending time is present" do
-        expect(media_access_event(event).completed)
+        expect(media_access_event.completed)
           .to be_truthy
       end
 
       it "returns falsey if ending time is not present" do
         event = instance_double(MediaAccessEvent, end_time: nil)
 
-        expect(media_access_event(event).completed)
+        expect(MediaAccessEventPresenter.new(event: event, start_date: Date.today).completed)
           .to be_falsey
       end
     end
 
     describe "#available_on" do
       it "returns the date of availability" do
-        allow(event)
-          .to receive_message_chain("participant.active_membership.start_date")
-          .and_return(Date.today)
-
-        expect(media_access_event(event).available_on)
+        expect(media_access_event.available_on)
           .to eq((Date.today + 2.days).to_s(:user_date))
       end
     end
 
     describe "#duration_of_session" do
       it "returns the length of the event" do
-        expect(media_access_event(event).duration_of_session)
+        expect(media_access_event.duration_of_session)
           .to be_within(0.1).of(3600)
       end
     end
@@ -50,18 +46,15 @@ module ThinkFeelDoEngine
     describe "#sortable" do
       it "returns the number of seconds since the event was started" do
         event = instance_double(MediaAccessEvent, task_release_day: 2)
-        allow(event)
-          .to receive_message_chain("participant.active_membership.start_date")
-          .and_return(Date.parse("2015-01-21"))
 
-        expect(media_access_event(event).sortable)
+        expect(MediaAccessEventPresenter.new(event: event, start_date: Date.today).sortable)
           .to eq Date.parse("2015-01-22").to_time.to_i
       end
     end
 
     describe "#title" do
       it "returns slide's title" do
-        expect(media_access_event(event).title)
+        expect(media_access_event.title)
           .to eq "foo"
       end
     end
