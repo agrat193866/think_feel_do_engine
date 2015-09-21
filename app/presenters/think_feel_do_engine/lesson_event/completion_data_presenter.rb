87a626348cc6c3969441e1dffbd8b7ac910333ac
michaelwehrley <michaelwehrley@gmail.com>
2015-09-21 16:38:06 -0500

    Fix Duration Bug
    * Add correct parameters for displaying distance in words
    * Update view to use presenter to remove logic from the view
    * Add view specs and presenter specs.
    
    [Finished: #102591154]

---
+++ app/presenters/think_feel_do_engine/lesson_event/completion_data_presenter.rb
@@ -0,0 +1,39 @@
+module ThinkFeelDoEngine
+  module LessonEvent
+    # Creates a presenter for the logic of the completion data partial
+    class CompletionDataPresenter
+      attr_reader :event, :lesson_module
+
+      delegate :pretty_title, to: :lesson
+
+      def initialize(event:, lesson_module:)
+        @event = event
+        @lesson_module = lesson_module
+      end
+
+      def last_page_number_opened
+        event[:last_page_number_opened]
+      end
+
+      def last_page_opened_at
+        DateTime.iso8601(event[:last_page_opened_at])
+      end
+
+      def page_headers
+        event[:page_headers].try do |page_headers|
+          page_headers[2]
+        end
+      end
+
+      def selected_at
+        DateTime.iso8601(event[:lesson_selected_at])
+      end
+
+      private
+
+      def lesson
+        lesson_module.find(event[:lesson_id])
+      end
+    end
+  end
+end
