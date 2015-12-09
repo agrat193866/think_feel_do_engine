979dbf2c5356e4b7aa0c152e090f2df35bfee648
agrat193866 <agrat193866@gmail.com>
2015-12-09 09:31:33 +0000

    Merge pull request #285 from cbitstech/ems-108985146
    Reduce password entropy to allow for simpler passwords.

--- spec/features/user/password_spec.rb
+++ spec/features/user/password_spec.rb
@@ -8,25 +8,10 @@ feature "reset user password", type: :feature do
   before do
     clear_emails
     visit "/users/sign_in"
+    navigate_to_change_password_page
   end
 
   it "should redirect after password update", :js do
-    click_on "Forgot your password?"
-
-    within "h2" do
-      expect(page).to have_text "Forgot your password"
-    end
-
-    fill_in "Email", with: "admin1@example.com"
-    click_on "Send me reset password instructions"
-
-    expect(page).to have_text "You will receive an email with instructions on how to reset your password in a few minutes."
-    expect(last_email.to).to include(users(:admin1).email)
-
-    visit extract_password_edit_path_from_email
-
-    expect(page).to have_text "Change your password"
-
     fill_in "New password", with: valid_password
     fill_in "Confirm new password", with: valid_password
 
@@ -36,7 +21,47 @@ feature "reset user password", type: :feature do
     expect(current_path).to eq "/privacy_policy"
   end
 
-  it "validates password", :js do
+  context "displays the correct password strength" do
+    it "displays 'Weak' with no password given", :js do
+      within "#password-strength .text-danger" do
+        expect(page).to have_text "Weak"
+      end
+    end
+
+    it "displays 'Medium' with a medium entropy password", :js do
+      fill_in "New password", with: "1Dog ca!"
+      fill_in "Confirm new password", with: "1Dog ca!"
+
+      within "#password-strength .text-primary" do
+        expect(page).to have_text "Medium"
+      end
+    end
+
+    it "shows 'Strong' with a strong entropy password", :js do
+      fill_in "New password", with: valid_password
+      fill_in "Confirm new password", with: valid_password
+
+      within "#password-strength .text-success" do
+        expect(page).to have_text "Strong"
+      end
+    end
+
+    it "shows \"password can't be blank\" when password is deleted", :js do
+      fill_in "New password", with: valid_password
+      fill_in "Confirm new password", with: valid_password
+
+      within "#password-strength .text-success" do
+        expect(page).to have_text "Strong"
+      end
+
+      fill_in "New password", with: ""
+      click_on "Change my password"
+
+      expect(page).to have_text "Password can't be blank"
+    end
+  end
+
+  def navigate_to_change_password_page
     click_on "Forgot your password?"
 
     within "h2" do
@@ -52,34 +77,5 @@ feature "reset user password", type: :feature do
     visit extract_password_edit_path_from_email
 
     expect(page).to have_text "Change your password"
-    within "#password-strength .text-danger" do
-      expect(page).to have_text "Weak"
-    end
-
-    fill_in "New password", with: "1Dog ca!"
-    fill_in "Confirm new password", with: "1Dog ca!"
-
-    within "#password-strength .text-primary" do
-      expect(page).to have_text "Medium"
-    end
-
-    fill_in "New password", with: valid_password
-    fill_in "Confirm new password", with: valid_password
-
-    within "#password-strength .text-success" do
-      expect(page).to have_text "Strong"
-    end
-
-    fill_in "New password", with: ""
-    click_on "Change my password"
-
-    expect(page).to have_text "Password can't be blank"
-
-    fill_in "New password", with: valid_password
-    fill_in "Confirm new password", with: valid_password
-
-    within "#password-strength .text-success" do
-      expect(page).to have_text "Strong"
-    end
   end
 end
